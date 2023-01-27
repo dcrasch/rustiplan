22ec9a5097fda396aad0087fe0992240b9a0f876
David Rasch <dcrasch@gmail.com>
2023-01-27 21:19:12 +0100

    refactor handles

--- src/components/cell.rs
+++ src/components/cell.rs
@@ -12,43 +12,48 @@ pub struct CellProps<'a> {
 
 pub fn Cell<'a>(cx: Scope<'a, CellProps<'a>>) -> Element {
     let is_editing = use_state(cx, || false);
-    let contents = use_state(cx, || cx.props.children.clone());
+    let content = use_state(cx, || cx.props.children.clone());
     let editing = if **is_editing { "editing" } else { "" };
+
+    let start_editing = move |_| {
+        cx.props.onselected.call(());
+        is_editing.set(true);
+    };
+
+    let blur = move |_| {
+        cx.props.onupdate.call(content.get().to_string());
+        is_editing.set(false);
+    };
+
+    let update_content = move |evt: Event<FormData>| content.set(evt.value.clone());
+
+    let keypress = move |evt: Event<KeyboardData>| {
+        if evt.key() == Key::Enter {
+            cx.props.onupdate.call(content.get().to_string());
+            is_editing.set(false);
+        }
+    };
+    let keydown = move |evt: Event<KeyboardData>| {
+        if evt.key() == Key::Escape {
+            content.set(cx.props.children.to_string());
+            is_editing.set(false);
+        }
+    };
 
     cx.render(rsx! {
     div {
         class: "cell {editing}",
-        onclick: move |_| {
-            cx.props.onselected.call(());
-            is_editing.set(true);
-        },
-        label { "{contents}"},
-        is_editing.then(|| {
-              rsx! {
-                input {
-                    value: "{contents.clone()}",
-                    oninput: move |evt| {
-                        contents.set(evt.value.clone());
-                    },
-            onblur: move |_| {
-            cx.props.onupdate.call(contents.get().to_string());
-            is_editing.set(false);
-            },
-            onkeypress: move |evt| {
-            match evt.key() {
-                Key::Escape => {
-                contents.set(cx.props.children.to_string());
-                is_editing.set(false);
-                },
-                Key::Enter => {
-                cx.props.onupdate.call(contents.get().to_string());
-                is_editing.set(false);
-                }
-                _ => {}
-            }
-            }
-                }
-            }
+        onclick: start_editing,
+        label { "{content}"},
+        is_editing.then(|| { rsx! {
+        input {
+                value: "{content}",
+                oninput: update_content,
+                onblur: blur,
+                onkeypress: keypress,
+        onkeydown: keydown,
+        }
+    }
         }),
     }
     })
