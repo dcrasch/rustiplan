2e46fc880ef0cf60e72c96d5ae73a49f0ca893f5
David Rasch <dcrasch@gmail.com>
2023-02-03 21:43:17 +0100

    add display value to cell

--- src/components/cell.rs
+++ src/components/cell.rs
@@ -1,6 +1,9 @@
 use dioxus::html::input_data::keyboard_types::Key;
 use dioxus::prelude::*;
 
+use crate::formula::{parse,parse_expr,eval};
+
+
 #[derive(Props)]
 pub struct CellProps<'a> {
     pub row: usize,
@@ -13,8 +16,18 @@ pub struct CellProps<'a> {
 pub fn Cell<'a>(cx: Scope<'a, CellProps<'a>>) -> Element {
     let is_editing = use_state(cx, || false);
     let content = use_state(cx, || cx.props.children.clone());
+    let display_value = use_state(cx, || cx.props.children.clone());    
     let editing = if **is_editing { "editing" } else { "" };
 
+    use_effect(cx,(content,display_value), |(content,display_value)| async move {
+	if let Ok(p) = parse(&content) {
+	    let dv = format!("{}",eval(&parse_expr(p)));
+	    if display_value!=dv {
+		display_value.set(dv);
+	    }
+	}
+    });
+    
     let start_editing = move |_| {
         cx.props.onselected.call(());
         is_editing.set(true);
@@ -44,7 +57,7 @@ pub fn Cell<'a>(cx: Scope<'a, CellProps<'a>>) -> Element {
     div {
         class: "cell {editing}",
         onclick: start_editing,
-        label { "{content}"},
+        label { "{display_value}"},
         is_editing.then(|| { rsx! {
         input {
                 value: "{content}",
